de93c964b560c59fadd87e0ea278c7da9e8654b3
Rahul <rahul@bigappcompany.in>
2019-08-18 21:50:17 +0530

    Added a new container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 
 const layout = (props) => {
     return (
         <Aux>
             <div>Toolbar, SideDrawer, BackDrop</div>
-            {props.children}
+            <main className={classes.Content}>
+                {props.children}
+            </main>
         </Aux>
     )
 }
