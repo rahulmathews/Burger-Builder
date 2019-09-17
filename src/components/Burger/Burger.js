9b55a6da750ecb66dd44e1e9d69ef740e566a91c
root <rahulmudulakar123@gmail.com>
2019-09-17 09:15:58 +0530

    added msg for empty ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,20 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'; 
 
 const burger = (props) => {
-    const transformIngredients = Object.keys(props.ingredients).map((igKey) => {
+    let transformIngredients = Object.keys(props.ingredients).map((igKey) => {
         let arr = [];
         for(let i = 0; i < props.ingredients[igKey]; i++){
             arr.push(<BurgerIngredient key={igKey + i} type={igKey}/>);
         }
         return arr;
-    })
+    }).reduce((arr, ele) => {
+        return arr.concat(ele);
+    }, []);
+
+    if(transformIngredients.length === 0){
+        transformIngredients = <p>Please start adding ingredients </p>;
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
